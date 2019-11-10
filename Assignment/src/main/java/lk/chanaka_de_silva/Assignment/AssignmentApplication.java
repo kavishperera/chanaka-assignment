d47002d08317dc3ce7f388b2afe9239b86a189f5
Kavish Manjitha <kavishmanjitha@gmail.com>
2019-11-10 23:37:06 +0530

    Configuration Bean Create

--- Assignment/src/main/java/lk/chanaka_de_silva/Assignment/AssignmentApplication.java
+++ Assignment/src/main/java/lk/chanaka_de_silva/Assignment/AssignmentApplication.java
@@ -1,14 +1,8 @@
 package lk.chanaka_de_silva.Assignment;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -18,15 +12,4 @@ public class AssignmentApplication {
         SpringApplication.run(AssignmentApplication.class, args);
     }
 
-    @Bean
-    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        MappingJackson2HttpMessageConverter converter
-                = new MappingJackson2HttpMessageConverter(mapper);
-        return converter;
-    }
 }
